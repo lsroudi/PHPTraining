a6b56fe21e25287ef3b237ecbb1a872be810367f
lsroudi <lsroudi@gmail.com>
2014-02-03 23:50:15 +0100

    refactor ClassLoader

--- PHPTraining/Autoloading/Autoload.php
+++ PHPTraining/Autoloading/Autoload.php
@@ -1,14 +1,14 @@
 <?php
 
 /**
- * Description of Autoload
+ * Description of ClassLoader
  *
  * (c) lsroudi <http://lsroudi.com/> <lsroudi@gmail.com>
  * 
  * For the full copyright and license information, please view the LICENSE
  * file that was distributed with this source code.
  */
-class Autoload {
+class ClassLoader {
 
     private $classPath;
 
@@ -21,18 +21,19 @@ class Autoload {
     public function autoload()
     {
         spl_autoload_register(function () {
-                    require_once'classes/' . $this->classPath . '.class.php';
+                    require_once'./Config/' . $this->classPath . '.php';
                 });
     }
 
     public function displayInludedFile()
     {
+        print(var_dump(Config::BAR));          // 10
         print(var_dump(get_required_files()));
     }
 
 }
 
-$autoload = new Autoload('/dossier/fichier');
+$autoload = new ClassLoader('Config');
 $autoload->displayInludedFile();
 ?>
 
---
+++ PHPTraining/Autoloading/Config/Config.php
@@ -0,0 +1,15 @@
+<?php
+
+/**
+ * Description of Config
+ *
+ * (c) lsroudi <http://lsroudi.com/> <lsroudi@gmail.com>
+ * 
+ * For the full copyright and license information, please view the LICENSE
+ * file that was distributed with this source code.
+ */
+class Config {
+    const BAR = 10;
+}
+?>
+
