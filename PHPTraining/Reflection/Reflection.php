02a7946f9855d917c614a6f0950ffdf2d28453ce
lsroudi <lsroudi@gmail.com>
2014-03-03 00:02:05 +0100

    add some test to ReflectionTest class

--- PHPTraining/Reflection/Reflection.php
+++ PHPTraining/Reflection/Reflection.php
@@ -8,8 +8,17 @@
  * For the full copyright and license information, please view the LICENSE
  * file that was distributed with this source code.
  */
-class Reflection {
+
+class ReflectionTest {
     //put your code here
+//    public function __toString()
+//    {
+//        echo 'my name is ReflectionTest';
+//    }
 }
+
+$classreflection = new ReflectionClass( 'ReflectionTest' );
+Reflection::export($classreflection);
+
 ?>
 
