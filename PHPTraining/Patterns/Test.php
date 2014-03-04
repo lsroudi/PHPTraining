f5d487efb7480f7c26372156261c599434a1d2c6
lsroudi <lsroudi@gmail.com>
2014-03-04 22:55:51 +0100

    create class to begin test

---
+++ PHPTraining/Patterns/Test.php
@@ -0,0 +1,15 @@
+<?php
+
+/**
+ * Description of Test
+ *
+ * (c) lsroudi <http://lsroudi.com/> <lsroudi@gmail.com>
+ * 
+ * For the full copyright and license information, please view the LICENSE
+ * file that was distributed with this source code.
+ */
+class Test {
+    //put your code here
+}
+?>
+
